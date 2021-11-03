bf00e7673b913af2641478a0d261a68b48debf4a
fsuleman2 <sydsulmn7@gmail.com>
2021-11-03 23:30:30 +0530

    backend base setup done

---
+++ CourseMangementApp/src/main/java/com/coursebackend/CourseMangementAppApplication.java
@@ -0,0 +1,13 @@
+package com.coursebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMangementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMangementAppApplication.class, args);
+	}
+
+}
